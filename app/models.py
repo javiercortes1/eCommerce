ca2cb61dd804269c8f4baee8d9f004ce82bc6972
darilokoh <darilokoh@hotmail.com>
2023-04-25 23:34:31 -0400

    model category/product
    models category/ product creados ,
    paleta de colores base.html para header, body y footer

--- app/models.py
+++ app/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.TextField(max_length=200)
+    new = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    cc = models.IntegerField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
