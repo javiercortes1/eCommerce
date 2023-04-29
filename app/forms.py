bd7617b25ea0c8ba4eb39e95e5df8d17e83a705b
darilokoh <darilokoh@hotmail.com>
2023-04-29 16:19:28 -0400

    add/list product
    se genera metodo add y list de product con su interfaz inicial

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Contact
+from .models import Contact, Product
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 
@@ -9,4 +9,28 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ["name", "email", "phone", "message", "queryType"]
         # fields = '__all__'
+        labels = {
+            'name': 'Nombre completo',
+            'email': 'Correo electrónico',
+            'phone': 'Teléfono',
+            'message': 'Mensaje',
+            'queryType': 'Tipo de consulta'
+        }
 
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        # fields = ["name", "price", "description", "new", "category","cc", "stock", "featured", "image"]
+        fields = '__all__'
+        labels = {
+            'name': 'Nombre',
+            'description': 'Descripcion',
+            'price': 'Precio',
+            'category': 'Categoria',
+            'cc': 'Cantidad(cc)',
+            'stock': 'Unidades',
+            'new': '¿Nuevo?',
+            'featured': 'Destacado?',
+            'image': 'Imagen'
+        }
--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, services, catalogue, contact
+from .views import home, services, catalogue, contact, add_product, list_product
 
 urlpatterns = [
     path('', home, name="home"),
     path('catalogue/', catalogue, name="catalogue"),
     path('services/', services, name="services"),
     path('contact/', contact, name="contact"),
+    path('add-product/', add_product, name="add_product"),
+    path('list-product/', list_product, name="list_product"),
 ]
