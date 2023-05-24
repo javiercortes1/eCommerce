5bc4bcc64c5fecf140b1dcaad79ed8630e0241d7
darilokoh <darilokoh@hotmail.com>
2023-05-24 01:13:37 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     stock = models.IntegerField()
     featured = models.BooleanField(default=False)
     image = models.ImageField(upload_to="products", null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
