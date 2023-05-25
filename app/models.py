bdbf45c5775562a33d1693cbd76393ea6d38c45a
darilokoh <darilokoh@hotmail.com>
2023-05-24 23:01:41 -0400

    model Rental y Barrel
    faltan views, form urls de barrel
    falta list.html y detail.html de Rental

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
@@ -9,6 +11,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
@@ -20,19 +23,19 @@ class Product(models.Model):
     featured = models.BooleanField(default=False)
     image = models.ImageField(upload_to="products", null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    rental_product = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-    
+
+
 class QueryType(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
 
     def __str__(self):
-        return self.name  
+        return self.name
 
-    
+
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -41,13 +44,39 @@ class Contact(models.Model):
     queryType = models.ForeignKey(QueryType, on_delete=models.PROTECT)
 
     def __str__(self):
-        return self.name     
+        return self.name
+
+
+class Barrel(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Rental(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    barrels = models.ManyToManyField(Barrel)
     start_date = models.DateField()
     end_date = models.DateField()
     status = models.CharField(max_length=20)
     deposit_paid = models.BooleanField(default=False)
 
+    def clean(self):
+        if self.start_date > self.end_date:
+            raise ValidationError(
+                "La fecha de inicio no puede ser posterior a la fecha de finalización.")
+
+    def get_duration(self):
+        return (self.end_date - self.start_date).days
+
+    @property
+    def is_active(self):
+        today = timezone.now().date()
+        return self.start_date <= today <= self.end_date
+
+    def __str__(self):
+        return f"{self.user.username} - Rental {self.pk}"
